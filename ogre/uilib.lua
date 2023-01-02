20889b8cf3280ce7d92a112a9dd8ee1ba37f1cc3
blender <105875726+blendet@users.noreply.github.com>
2023-01-02 21:08:48 +0000

    Update uilib.lua

--- ogre/uilib.lua
+++ ogre/uilib.lua
@@ -5,6 +5,8 @@ function library:CreateWindow(name)
 	local window = Instance.new("Frame")
 	local title = Instance.new("Frame")
 	local TextLabel = Instance.new("TextLabel")
+	local tabs = Instance.new("Frame")
+	local TextButton = Instance.new("TextButton")
 	
 	ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 	ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
@@ -31,8 +33,22 @@ function library:CreateWindow(name)
 	TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
 	TextLabel.TextSize = 14.000
 	TextLabel.TextXAlignment = Enum.TextXAlignment.Left
-
-
+	
+	function library:CreateTab(name)
+		tabs.Name = "tabs"
+		tabs.Parent = window
+		tabs.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		tabs.Position = UDim2.new(0.0159680638, 0, 0.0568720363, 0)
+		tabs.Size = UDim2.new(0, 485, 0, 22)
+		
+		TextButton.Parent = tabs
+		TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+		TextButton.Size = UDim2.new(0, 70, 0, 22)
+		TextButton.Font = Enum.Font.Code
+		TextButton.Text = name
+		TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+		TextButton.TextSize = 14.000
+	end
 end
 
 return library
